4f2885f9f6f12002adc88c499183a26fcf35eb13
ryanlinjui <ryanlinjui@gmail.com>
2025-02-14 12:31:42 +0800

    chore: openai api key input

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ load_dotenv()
 class Config:
     GCP_CREDENTIALS = os.environ.get("GCP_CREDENTIALS")
     GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
